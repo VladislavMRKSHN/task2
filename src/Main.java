a82e0b19fe328b93bc726ef1cae5897652dfda6e
Владислав <vmarkushin04@mail.ru>
2025-02-04 12:44:12 +0700

    Исправлены ошибки в Task1 и Task2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        double dollar;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Запрос суммы
+        System.out.print("Введите сумму в долларах: ");
+        dollar = scanner.nextDouble();
+
+        // Проверка, чтобы сумма была положительная
+        if (dollar < 0) {
+            System.out.println("Сумма не может быть отрицательна. Введите положительное значение.");
+            return;
         }
+
+        // Вычисление
+        double euro = dollar * 0.85;
+
+        // Вывод результата
+        System.out.println("Сумма долларов " + dollar + " равна " + euro + " евро");
     }
 }
